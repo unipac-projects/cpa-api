34f3cb7fa02be742c5ae4561fdd4055ecf1102de
Johny Messias <3jmessias@gmail.com>
2019-09-08 19:14:23 -0300

    Modificacao em >:migration/V1_10question.sql

--- src/main/resources/db/migration/V1_10__question.sql
+++ src/main/resources/db/migration/V1_10__question.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS `discipline` (
+CREATE TABLE IF NOT EXISTS `question` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(150) NOT NULL,
+  `title` varchar(150) NOT NULL,
   `description` varchar(150) NOT NULL,
-  `client_id` bigint(20) NOT NULL,
   `create_by` varchar(255) NOT NULL DEFAULT 'system_user',
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_modified_by` varchar(255),
