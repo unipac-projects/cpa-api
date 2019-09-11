64d4e104ce3dd8442f2b10a389d7f606343f8cd0
Johny Messias <3jmessias@gmail.com>
2019-09-11 00:42:35 -0300

    Correção da QuestionRequestConverter

--- src/main/java/br/com/unipac/cpa/web/convert/request/QuestionRequestConverter.java
+++ src/main/java/br/com/unipac/cpa/web/convert/request/QuestionRequestConverter.java
@@ -1,29 +1,28 @@
 package br.com.unipac.cpa.web.convert.request;
 
+import br.com.unipac.cpa.model.domain.Evaluation;
 import br.com.unipac.cpa.model.domain.Question;
+import br.com.unipac.cpa.model.service.impl.EvaluationServiceImpl;
 import br.com.unipac.cpa.web.dto.request.QuestionRequest;
-import br.com.unipac.cpa.model.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.Converter;
 import java.util.Optional;
 
 @Component
 public class QuestionRequestConverter implements Converter<QuestionRequest, Question> {
 
     @Autowired
-    private OpcaoRepository opcaoRepository;
+    private EvaluationServiceImpl evaluationRepository;
 
     @Override
-    public Question converter(QuestionRequest questionRequest){
-        Question question = new Question();
+    public Question convert(QuestionRequest questionRequest) {
+        Question question =  new Question();
         question.setTitle(questionRequest.getTitle());
         question.setDescription(questionRequest.getDescription());
-//        Optional<>
-//        question.setEvaluation();
-//        question.setScala_likert();
+        Optional<Evaluation> evaluation = evaluationRepository.findById(questionRequest.getId());
+        question.setEvaluation(String.valueOf(evaluation.get()));
         return question;
     }
 }
