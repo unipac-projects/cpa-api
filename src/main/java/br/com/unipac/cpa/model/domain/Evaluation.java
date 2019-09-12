d0a8a8d0dcde9bb78bef7bc99cec3721ecbf6bff
Iago Santos <iagofalcom@hotmail.com>
2019-09-12 02:14:54 -0300

    Impl Relacionamento entre Evaluation e Question

--- src/main/java/br/com/unipac/cpa/model/domain/Evaluation.java
+++ src/main/java/br/com/unipac/cpa/model/domain/Evaluation.java
@@ -3,13 +3,11 @@ package br.com.unipac.cpa.model.domain;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "evaluation")
@@ -41,10 +39,16 @@ public class Evaluation extends AudityEntity {
     @JoinColumn(name = "course_id", referencedColumnName = "id")
     private Course course;
 
+    @OneToMany(mappedBy = "id")
+    @Getter
+    @Setter
+    private List<Question> questions;
+
     public void update(Evaluation evaluation) {
         this.name = evaluation.getName();
         this.description = evaluation.getDescription();
         this.date = evaluation.getDate();
         this.course = evaluation.getCourse();
+        this.questions = evaluation.getQuestions();
     }
 }
